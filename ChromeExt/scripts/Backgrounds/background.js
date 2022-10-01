3b313f75f9b13c0e7bf2088c98781c29ce7e4a9e
amimer nabil <amimernabil@gmail.com>
2022-10-01 14:19:59 -0400

    completed manifest.json with needed permissions, content directories, and scripts for the extensions, added basic request from backend to content script for testing

--- ChromeExt/scripts/Backgrounds/background.js
+++ ChromeExt/scripts/Backgrounds/background.js
@@ -1,1 +1,24 @@
 console.log("background running")
+
+chrome.commands.onCommand.addListener(async (command) => {
+   if(command === 'getDefinitions'){
+     await getDefinition(); 
+   }
+})
+
+// function to get definition of word
+const getDefinition = async () => {
+   chrome.tabs.query({active: true, currentWindow: true}, async function (tabs){
+      let message = {
+         method : "get-highlighted-word"
+      }
+
+      // now that we have the tab ID, we send a message from it, to its content script
+      chrome.tabs.sendMessage(tabs[0].id, message, async function(response){
+        console.log(response);
+      });
+   });
+}
+
+
+
--- ChromeExt/scripts/Content/content.js
+++ ChromeExt/scripts/Content/content.js
@@ -1,2 +1,13 @@
 console.log("smartTracker is active...")
 
+// onMessage Listeners
+chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
+    if (request.method === 'get-highlighted-word') {
+        sendResponse("Hello this is from Content")
+    }
+    return true;
+});
+
+
+
+
